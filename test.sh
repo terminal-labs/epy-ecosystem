e77fa3f8f52823f1ce9c9bac1283c250227e0731
Michael Verhulst <michael@terminallabs.com>
2022-04-29 13:12:11 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+pip install examples/input/change
+sleep 5
+pip install examples/input/changefast
+sleep 5
+changefast commands epycalc
+sleep 5
+runnercompilepython runner run
+sleep 5
+runnercompilepython runner pbar
+sleep 5
