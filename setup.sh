56397cbc86b2040b6d51827a1fc7510aeb591633
Michael Verhulst <michael@terminallabs.com>
2022-04-29 01:34:45 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,13 @@ cd deps
 git clone git@github.com:terminal-labs/epython-wip.git
 git clone git@github.com:terminal-labs/epy-lib-wip.git
 git clone git@github.com:terminal-labs/epy-setup-wip.git
-git clone https://github.com/terminal-labs/job-light-runner.git
+
+if [ -z "$GITHUB_ACTIONS" ]
+then
+    git clone https://github.com/terminal-labs/job-light-runner.git
+else 
+    git clone git@github.com:terminal-labs/job-light-runner.git
+fi
 pip install -e epython-wip
 pip install -e epy-lib-wip
 pip install -e epy-setup-wip
